97f2373e4fe842aa2846cdd9eb8d40a16ac9ee04
rdhwan <fathanridhwann@gmail.com>
2023-11-19 03:10:14 +0700

    [chore]: add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "KITAB API", // Name of your application
+  script: "src/index.ts", // Entry point of your application
+  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+};
